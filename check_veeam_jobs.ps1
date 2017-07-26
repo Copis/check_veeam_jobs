3b4e8c1ac22cae7fd43f1136e13b8fd957404f05
Xavier Vallve <bikermk36@hotmail.com>
2017-07-26 15:46:35 +0200

    Add files via upload

---
+++ check_veeam_jobs.ps1
@@ -0,0 +1,27 @@
+﻿
+
+# Parameters
+param(
+    [string]$Server,
+    [string]$user,
+    [string]$password,
+    [string]$pattern
+)
+
+#Adding required SnapIn
+if ((Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null) {
+	Add-PsSnapin VeeamPSSnapIn
+}
+
+#Get Veeam backup jobs
+if ($pattern -eq $null) {
+    $jobs = Get-VBRJob
+}
+else {
+    $pattern = "*$pattern*"
+    $jobs = Get-VBRJob -Name $pattern
+}
+
+foreach ($job in $jobs) {
+    Write-Host $job.Name
+    }
